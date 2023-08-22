38edc40eb0f20de78f370fa4ba9dc8fca5ca9339
Den Mor <kievden26092014@gmail.com>
2023-08-23 00:58:08 +0300

    feature: func to change notificaiton status

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -7,7 +7,10 @@ from telebot import types
 from telegram_bot.constants import BOT_TOKEN
 from db.db_api import Database
 from db.new_db_api import SqlAlchemyDataBaseApi
+from db.constants import NotificationStatus
+from db.models import ScheduledNotificationModel
 from telegram_bot.utils.validation_utils import is_time_format_valid, is_content_input_step_valid
+from utils_general.utils import week_data_data_transform
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -18,17 +21,17 @@ bot = telebot.TeleBot(BOT_TOKEN)
 messages_templates = \
     {
         '/start':
-            '''
-            --- BOT IS UNDER MAINTENANCE ---
-            Hey {user}, welcome to scheduler bot.
-            
-            Bot allows to create scheduled notifications. 
-            Use following commands to interact with bot.
-             
-            /getSchedule - to show your notification
-            /addSchedule - to add new scheduled notification 
-            /deleteScheduler - to delete specific scheduler
-            ''',
+        '''
+--- BOT IS UNDER MAINTENANCE ---
+Hey {user}, welcome to scheduler bot.
+
+Bot allows to create scheduled notifications. 
+Use following commands to interact with bot.
+ 
+/addSchedule - to add new scheduled notification 
+/statusSchedule - to update, delete, pause or activate your scheduled notifications
+/getSchedule - to show your notification (temporary)
+        ''',
         '/help':
             '''
         
@@ -56,12 +59,8 @@ messages_templates = \
                     Your schedule successfully updated 👍
                     '''
             },
-        '/updateScheduler':
-            '''
-            ''',
-        '/deleteScheduler':
-            '''
-            Input the following id of schedule that you want to delete:\n{schedule} 
+        '/statusSchedule':
+            ''' 
             '''
     }
 
@@ -95,11 +94,7 @@ class ScheduledNotification:
 
 
 user_schedule_dict: dict[str, ScheduledNotification] = {}
-
-
-def send_message(message=None):
-    if message:
-        bot.send_message(418980357, message)
+user_notification_data: dict[str, list] = {}
 
 
 def get_menu_keyboard_markup() -> types.ReplyKeyboardMarkup:
@@ -111,7 +106,7 @@ def get_menu_keyboard_markup() -> types.ReplyKeyboardMarkup:
 
 
 # TODO: save user
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help', 'info'])
 def start(message):
     message_text: str = messages_templates.get('/start', 'undefined')
     message_data: dict = {'user': message.from_user.username}
@@ -119,22 +114,24 @@ def start(message):
     bot.send_message(message.from_user.id, message_text.format(**message_data), reply_markup=get_menu_keyboard_markup())
 
 
-# TODO: change logic (sqlalchemy usage)
-
+# TODO: mb delete
 @bot.message_handler(commands=['getSchedule'])
 def getSchedule(message):
     message_text: str = messages_templates.get('/getSchedule', 'undefined')
     message_data: dict = {}
 
-    schedule_data = get_schedules(message.from_user.id)
+    tl_user_id = message.from_user.id
+
+    db_api = SqlAlchemyDataBaseApi()
+    user_scheduled_data = db_api.get_notification_by_tl_user_id(tl_user_id)
 
     processed_data = ''
-    for idx, data in enumerate(schedule_data):
-        schedule_data = json.loads(data[2])
-        week_days_data: list = schedule_data.get("week_days", ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
-        processed_data += f"\n ***#{idx} - *** {schedule_data['content']}\n" \
-                          f"> {schedule_data['time']}\n" \
-                          f" {', '.join(week_days_data)}\n" + '-' * 10 + '\n'
+    for idx, notification_data in enumerate(user_scheduled_data):
+        settings_data: dict = json.loads(notification_data.settings)
+        week_days_data: list = week_data_data_transform(settings_data.get('week_days', ''))
+        processed_data += f"\n ***[#{idx}] - status: {notification_data.status}*** \n{settings_data.get('content', '')}\n\n" \
+                          f"> {settings_data.get('time', '')}\n" \
+                          f" {', '.join(week_days_data)}\n" + '=' * 10 + '\n\n'
 
     message_data['schedule'] = processed_data
     bot.send_message(message.from_user.id, message_text.format(**message_data), parse_mode="Markdown")
@@ -173,9 +170,6 @@ def cancel_operation_wrapper(func):
 
 @cancel_operation_wrapper
 def process_time_step(message):
-    # markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    # markup.add('Male', 'Female')
-
     chat_id = message.chat.id
     tl_user_id = message.from_user.id
     key: str = str(chat_id) + str(tl_user_id)
@@ -235,7 +229,7 @@ def get_week_days_keyboard_markup(options: dict[int, str] = None):
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("selected_day_data:"))
-def week_days_button_click(call):
+def week_days_button_click_callback(call):
     markup = call.message.reply_markup
     old_keyboard = markup.keyboard
     # TODO: rethink saving approach
@@ -297,11 +291,13 @@ def process_data_save_step(call):
             username=call.from_user.username
         )
 
-    settings = json.dumps({'week_days': notification_data.days, 'time': notification_data.time})
+    settings = json.dumps(
+        {'content': notification_data.content, 'week_days': notification_data.days, 'time': notification_data.time})
     db_api.create_notification(settings=settings, tl_user_id=tl_user_id, user_id=user.id)
     del db_api
 
-    bot.reply_to(call.message, messages_templates['/addSchedule'][3], parse_mode="Markdown")
+    bot.send_message(call.from_user.id, messages_templates['/addSchedule'][3],
+                     parse_mode="Markdown", reply_markup=get_menu_keyboard_markup())
 
 
 def save_scheduled_data(content):
@@ -319,14 +315,107 @@ def save_scheduled_data(content):
     db.insert_row(Database.Tables.scheduler_test, schedule_data)
 
 
-# TODO: add logic
+def get_notification_status_change_markup(notification_id: int, current_notification_idx: int):
+    markup = types.InlineKeyboardMarkup()
+    item1 = types.InlineKeyboardButton('🗑 Delete',
+                                       callback_data=f'change_status:{NotificationStatus.ARCHIVED.value},{notification_id}')
+    item2 = types.InlineKeyboardButton('⛔ Suspend',
+                                       callback_data=f'change_status:{NotificationStatus.PAUSED.value},{notification_id}')
+    item3 = types.InlineKeyboardButton('🟢 Activate',
+                                       callback_data=f'change_status:{NotificationStatus.ACTIVE.value},{notification_id}')
+    item4 = types.InlineKeyboardButton('Previous',
+                                       callback_data=f'notification_selection:PREVIOUS,{current_notification_idx}')
 
-@bot.message_handler(commands=['delEvent'])
-def delete_event_reminder(message):
-    pass
+    item5 = types.InlineKeyboardButton('Next',
+                                       callback_data=f'notification_selection:NEXT,{current_notification_idx}')
+
+    markup.add(item1, item2, item3, item4, item5)
+    return markup
 
 
-# @bot.message_handler(func=lambda message: 'Cancel' in message.text)
+def get_str_notification_data(notification_obj: ScheduledNotificationModel, idx: int = 0):
+    settings_data: dict = json.loads(notification_obj.settings)
+    week_days_data: list = week_data_data_transform(settings_data.get('week_days', ''))
+    content = f"\n ***[#{idx}] - status: {notification_obj.status.value}*** \n{settings_data.get('content', '')}\n\n" \
+              f"> {settings_data.get('time', '')}\n " \
+              f"{', '.join(week_days_data)}\n" + '=' * 10 + '\n\n'
+    return content
+
+
+# TODO: redo - repeated code parts
+@bot.message_handler(commands=['statusSchedule'])
+def statusSchedule(message):
+    # message_text: str = messages_templates.get('/deleteSchedule', 'undefined')
+    # message_data: dict = {}
+
+    chat_id = message.chat.id
+    tl_user_id = message.from_user.id
+    key: str = str(chat_id) + str(tl_user_id)
+
+    db_api = SqlAlchemyDataBaseApi()
+    user_scheduled_data = db_api.get_notification_by_tl_user_id(tl_user_id)
+
+    processed_data_sap: list[dict] = []
+    for idx, notification_data in enumerate(user_scheduled_data):
+        content = get_str_notification_data(notification_obj=notification_data, idx=idx)
+        processed_data_sap_dict = {'notification_id': notification_data.id, 'content': content}
+        processed_data_sap.append(processed_data_sap_dict)
+
+    user_notification_data[key] = processed_data_sap
+
+    if processed_data_sap:
+        bot.send_message(message.from_user.id, processed_data_sap[0]['content'],
+                         parse_mode="Markdown", reply_markup=get_notification_status_change_markup(
+                processed_data_sap[0]['notification_id'], 0))
+    else:
+        bot.send_message(message.from_user.id, 'You do not have scheduled notifications', parse_mode="Markdown")
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("notification_selection:"))
+def notification_choosing_button_callback(call):
+    selected_notification_selection_data: list[str] = call.data.replace('notification_selection:', '').split(',')
+    # TODO: redo
+    selection_action: str = str(selected_notification_selection_data[0])
+    selection_current_notification_idx: int = int(selected_notification_selection_data[1])
+
+    chat_id = call.message.chat.id
+    tl_user_id = call.from_user.id
+    key: str = str(chat_id) + str(tl_user_id)
+    user_scheduled_data = user_notification_data[key]
+
+    new_notification_idx = 0
+    if selection_action == 'PREVIOUS':
+        new_notification_idx = (selection_current_notification_idx - 1) % len(user_scheduled_data)
+    elif selection_action == 'NEXT':
+        new_notification_idx = (selection_current_notification_idx + 1) % len(user_scheduled_data)
+
+    next_notification_data = user_scheduled_data[new_notification_idx]
+
+    bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text=next_notification_data['content'],
+        reply_markup=get_notification_status_change_markup(next_notification_data['notification_id'], new_notification_idx),
+        parse_mode="Markdown")
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("change_status:"))
+def change_notification_status_button_callback(call):
+    selected_status_data: list[str] = call.data.replace('change_status:', '').split(',')
+    selected_status: str = selected_status_data[0]
+    selected_notification_id: int = int(selected_status_data[1])
+    db_api = SqlAlchemyDataBaseApi()
+    db_api.change_notification_status(selected_notification_id, selected_status)
+
+    match selected_status:
+        case NotificationStatus.ACTIVE.value:
+            bot.answer_callback_query(call.id, 'Was Activated', show_alert=True)
+        case NotificationStatus.PAUSED.value:
+            bot.answer_callback_query(call.id, 'Was Paused', show_alert=True)
+        case NotificationStatus.ARCHIVED.value:
+            bot.answer_callback_query(call.id, 'Was Deleted', show_alert=True)
+
+
 @bot.message_handler(content_types=['text'])
 def menu_button_func(message):
     if message.text == ButtonsText.ADD_SCHEDULED_NOTIFICATION:
